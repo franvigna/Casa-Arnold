42422a202a116018cad626436921a648817c13fa
Fran Vigna <vignardel.francisco@gmail.com>
2022-08-19 14:31:42 -0300

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <>
+      <h1>Bienvenido a Casa Arnold</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
